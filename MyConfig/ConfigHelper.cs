c315b6819481ecb2f4a4e096718bf23a480c0a6b
11094 <1109465339@qq.com>
2026-01-10 11:27:24 +0800

    feat:新增了配置文件库

---
+++ MyConfig/ConfigHelper.cs
@@ -0,0 +1,79 @@
+﻿using HandyControl.Controls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml;
+
+namespace MyConfig
+{
+    public class ConfigHelper
+    {
+
+        private string _configPath;
+        public Dictionary<string, object> Settings { get; private set; } = new Dictionary<string, object>();
+        public JObject _configJson;
+        private static readonly object _cfgLock = new();
+
+        public void ConfigInit()
+        {
+            if (!File.Exists(_configPath))
+                throw new FileNotFoundException("找不到配置文件: " + _configPath);
+
+            var json = File.ReadAllText(_configPath);
+            Settings = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+
+            _configJson = JObject.Parse(File.ReadAllText(_configPath));
+        }
+        public bool SetConfig(string key, string value)
+        {
+            if (string.IsNullOrWhiteSpace(key)) return false;
+
+            lock (_cfgLock)
+            {
+                if (Settings == null || Settings.Count == 0) ConfigInit();
+
+                if (!Settings.ContainsKey(key)) return false;          // 只改不增
+                if (Settings[key] == value) return true;               // 无变化不写盘
+
+                Settings[key] = value;
+
+                var json = JsonConvert.SerializeObject(Settings, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(_configPath, json, new System.Text.UTF8Encoding(false));
+                return true;
+            }
+        }
+        public void SaveConfig()
+        {
+            File.WriteAllText(_configPath, _configJson.ToString(Newtonsoft.Json.Formatting.Indented));
+        }
+        public ConfigHelper(string configPath = "config.json")
+        {
+            _configPath = configPath;
+            ConfigInit();
+        }
+        public string GetValue(string? key)
+        {
+            if (key == null)
+            {
+                return "-";
+            }
+            if (Settings.TryGetValue(key, out var value))
+                return value?.ToString();
+            throw new KeyNotFoundException($"配置项中未找到键：{key}");
+        }
+
+
+
+
+    }
+}
---
+++ MyConfig/Controls/TextDialog.xaml.cs
@@ -0,0 +1,110 @@
+﻿using Core;
+using HandyControl;
+using HandyControl.Controls;
+using MyConfig;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyConfig.Controls;
+
+public partial class TextDialog
+{
+    ConfigHelper configHelper;
+    public TextDialog(string parameter, ConfigHelper _configHelper)
+    {
+        InitializeComponent();
+        Parameter = parameter;
+        DataContext = this;
+        configHelper = _configHelper;
+        LoadIpNodes();
+    }
+
+    public ICommand ConfirmCommand
+    {
+        get => (ICommand)GetValue(ConfirmCommandProperty);
+        set => SetValue(ConfirmCommandProperty, value);
+    }
+
+    public string Parameter { get; set; }
+
+    public static readonly DependencyProperty ConfirmCommandProperty =
+        DependencyProperty.Register(nameof(ConfirmCommand), typeof(ICommand), typeof(TextDialog), new PropertyMetadata(null));
+    public ObservableCollection<IpNode> IpNodes { get; set; } = new();
+
+    private void LoadIpNodes()
+    {
+        if (configHelper != null)
+        {
+            foreach (var property in configHelper._configJson.Properties())
+            {
+                string key = property.Name;
+                if (key.StartsWith($"_{Parameter}上")|| key.StartsWith($"_{Parameter}下") || key.StartsWith($"_{Parameter}翻") || key.StartsWith($"_{Parameter}周") || key.StartsWith($"{Parameter}_") )
+                {
+                    IpNodes.Add(new IpNode
+                    {
+                        Key = key,
+                        Value = property.Value?.ToString()
+                    });
+                }
+            }
+
+        }
+    }
+}
+
+public static class MyConfigCommand
+{
+    public static ConfigHelper? configHelper;
+    private static HandyControl.Controls.Dialog dialog;
+    public static void ShowText(string element)
+    {
+        if (configHelper == null)
+            throw new ArgumentNullException(nameof(configHelper) + "还没有将configHelper传入");
+        dialog = HandyControl.Controls.Dialog.Show(new TextDialog(element, configHelper) { ConfirmCommand = ConfirmCommand });
+    }
+    public static event Action? Confirmed;
+
+    public static ICommand ConfirmCommand => new RelayCommand<object>(OnConfirm);
+
+    private static void OnConfirm(object param)
+    {
+        if (param is ObservableCollection<IpNode> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                configHelper._configJson[node.Key] = node.Value;
+            }
+            configHelper.SaveConfig();
+            dialog.Close();
+
+            // 👇 触发事件
+            Confirmed?.Invoke();
+        }
+    }
+
+
+}
+
+public class IpNode : INotifyPropertyChanged
+{
+    private string _value;
+
+    public string Key { get; set; }
+    public string Value
+    {
+        get => _value;
+        set
+        {
+            if (_value != value)
+            {
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
